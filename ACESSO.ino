bb98561994fa95b1fd8d546960a4cfbcf6cae779
Jhonny0079 <Jhonnybf14@gmail.com>
2017-08-06 20:23:11 -0300

    Add files via upload

---
+++ ACESSO.ino
@@ -0,0 +1,107 @@
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3, POSITIVE);
+
+ 
+char st[20];
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Inicia a serial
+  SPI.begin();      // Inicia  SPI bus
+  mfrc522.PCD_Init();   // Inicia MFRC522
+  Serial.println("Aproxime o seu cartao do leitor...");
+  Serial.println();
+  pinMode(8, OUTPUT);
+  //Define o número de colunas e linhas do LCD:  
+  lcd.begin(16, 2);  
+  mensageminicial();
+}
+ 
+void loop() 
+{
+
+  
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Mostra UID na serial
+  Serial.print("UID da tag :");
+  String conteudo= "";
+  byte letra;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Mensagem : ");
+  conteudo.toUpperCase();
+  if (conteudo.substring(1) == "84 65 50 FF") //UID 1 - Chaveiro
+  {
+    acessoLiberado();
+  } else {
+    
+    acessoNegado();
+    
+    
+    }
+ 
+  
+} 
+ 
+void mensageminicial()
+{
+  lcd.clear();
+  lcd.print(" Aproxime o seu");  
+  lcd.setCursor(0,1);
+  lcd.print("cartao do leitor");  
+}
+
+ void acessoNegado() {
+  
+    Serial.println("Ola Cartao !");
+    Serial.println();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Ola Cartao !");
+    lcd.setCursor(0,1);
+    lcd.print("Acesso Negado !");
+    delay(3000);
+    mensageminicial();
+  }
+
+ void acessoLiberado () {
+    Serial.println("Ola Jhonny ");
+    Serial.println();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Ola Jhonny");
+    lcd.setCursor(0,1);
+    lcd.print("Acesso liberado!");
+    digitalWrite(8, HIGH);
+    delay(3000);
+    mensageminicial();
+  
+  
+  
+  }
